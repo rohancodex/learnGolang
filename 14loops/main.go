3f925dd69c040b0b7488f769259e7a57c8c557e6
rohancodex <rohancodex@gmail.com>
2021-11-13 12:17:50 +0530

    loops

--- 14loops/main.go
+++ 14loops/main.go
@@ -4,5 +4,36 @@ import "fmt"
 
 func main() {
 	fmt.Println("loops in golang")
+	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+	fmt.Println(days)
 
+	// for i := 0; i < len(days); i++ {
+	// 	fmt.Println(days[i])
+	// }
+
+	// for i := range days {
+	// 	fmt.Println(days[i])
+	// }
+
+	for _, day := range days {
+		fmt.Printf("value is %v\n", day)
+	}
+
+	value := 1
+	for value < 5 {
+
+		if value == 4 {
+			goto fun
+		}
+
+		if value == 3 {
+			value++
+			continue
+		}
+		fmt.Println("Value is: ", value)
+		value++
+	}
+
+fun:
+	fmt.Println("Fun Stuff")
 }
